94c192a5e56a409b146f94aea9f1f209cf525c56
Riley Jenum <riley.jenum@spacemushrooms.it>
2024-10-17 10:26:35 +0200

    Initial Commit

---
+++ virtual-library/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  virtual-library
+//
+//  Created by Riley Jenum on 17/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ virtual-library/virtual_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtual_libraryApp.swift
+//  virtual-library
+//
+//  Created by Riley Jenum on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct virtual_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
